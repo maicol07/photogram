4ab43b42eca3fb14e4986719e81abaedd36d2fa0
Maicol Battistini <maicol.battistini@studio.unibo.it>
2023-01-03 10:09:35 +0100

    build: Added Vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: ['resources/scss/app.scss', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+});
