cee6894c6bacd99c213b5f796fbdce9b41fbf200
Simone Redighieri <simone.redighieri@studio.unibo.it>
2023-01-04 10:38:22 +0100

    feat: ✨ Added image preview and better upload form

--- resources/views/livewire/user/dialog.blade.php
+++ resources/views/livewire/user/dialog.blade.php
@@ -1,7 +1,29 @@
 <div>
-    <span><input wire:model="image" type="file" id="edit-image" name="image"></span>
-    <span><label for="edit-image">Edit Image</label></span>
-    <div><x-textfield wire:model="bio" outlined textarea rows="8" cols="40" maxlength="140"
+    @if ($image)
+        <div id="preview-image">
+            <span>
+                @lang('Image Preview'):
+            </span>
+            <span>
+                <img id="temporary-image" src="{{$image->temporaryUrl()}}" alt="preview image">
+            </span>
+        </div>
+    @endif
+    <div class="mdc-layout-grid__inner" id="dialog-layout">
+        <span class="mdc-layout-grid__cell dialog-label">
+            @lang('Update profile image'):
+        </span>
+        <div class="mdc-layout-grid__cell">
+            <x-button id="attachment-btn" outlined :label="__('Upload File')" icon="paperclip" />
+        </div>
+        <input wire:model="image" id="edit-image" name="image" type="file">
+        @error('image') <span class="error">{{$message}}</span>@enderror
+        <span class="mdc-layout-grid__cell dialog-label">
+            @lang('Uploaded file:')
+            <span id="attachments-display">{{$image?->getClientOriginalName()}}</span>
+        </span>
+    </div>
+    <div><x-textfield wire:model="user.bio" outlined textarea rows="8" cols="40" maxlength="140"
                       id="edit-bio" label="Edit Bio" /></div>
     <div class="mdc-dialog__actions">
         <x-button dialog-button variant="outlined" label="Cancel" wire:click="close" variant="text"
