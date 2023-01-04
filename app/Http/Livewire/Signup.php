b14894c9ba193dc44ddb7059097ac5b020d32844
Elia Bellavista <elia.bellavista@studio.unibo.it>
2023-01-04 10:39:33 +0100

    feat: ✨ Added signup validation and logic

--- app/Http/Livewire/Signup.php
+++ app/Http/Livewire/Signup.php
@@ -1,19 +1,22 @@
 <?php
 
 namespace App\Http\Livewire;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Signup extends Page
 {
-    public string $name='';
-    public string $surname='';
-    public string $email='';
+    public string $name = '';
+    public string $surname = '';
+    public string $email = '';
     public string $dateOfBirth = '';
-    public string $username='';
-    public string $password='';
-    public string $repeatPassword='';
+    public string $username = '';
+    public string $password = '';
+    public string $repeatPassword = '';
 
     public $rules = [
         'name' => 'required|string',
@@ -22,21 +25,28 @@ class Signup extends Page
         'dateOfBirth' => 'required|date',
         'username' => 'required|max:20|min:4|string|unique:App\Models\User,username',
         'password' => 'required|max:24|min:8|string',
+        'repeatPassword' => 'required|same:password',
     ];
 
     public function signup(): void
     {
         $validatedData = $this->validate(attributes: [
-            "username" => "Username",
-            "password" => "Password",
-            "name" => "Name",
-            "surname" => "Surname",
-            "email" => "Email",
-            "dateOfBirth" => "Date of birth"
+            "username" => __("Username"),
+            "password" => __("Password"),
+            "name" => __("Name"),
+            "surname" => __("Surname"),
+            "email" => __("Email"),
+            "dateOfBirth" => __("Date of birth"),
+            "repeatPassword" => __("Repeated Password"),
         ]);
 
-
-
+        $user = new User();
+        $user->name = $validatedData['name'];
+        $user->surname = $validatedData['surname'];
+        $user->email = $validatedData['email'];
+        $user->username = $validatedData['username'];
+        $user->password = Hash::make($validatedData['password']);
+        $user->save();
     }
 
     public function page(): View
