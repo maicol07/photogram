1b4bffe2bff1a11ba1d9d10dc61c708cff923239
Elia Bellavista <elia.bellavista@studio.unibo.it>
2023-01-09 19:40:05 +0100

    feat: Completed setup page

--- resources/views/livewire/signup.blade.php
+++ resources/views/livewire/signup.blade.php
@@ -1,48 +1,35 @@
-<div style="text-align: center">
-    <div class="mdc-card mdc-card--outlined" id="login-card">
+<div class="auth-container">
+    <div class="mdc-card mdc-card--outlined auth-card" >
         <div>
-            <form wire:submit.prevent="signup">
-
-                <x-textfield :label="__('Name')" type="text" id="name" {{--wire:model="username"--}} />
-                <br/>
-                @error('name') <span class="error">{{ $message }}</span> @enderror
+            <form wire:submit.prevent="signupButton" class="auth-form">
+                <x-textfield :label="__('Name')" type="text" id="name" wire:model="name"/>
                 <br/>
 
-                <x-textfield :label="__('Surname')" type="text" id="surname" {{--wire:model="username"--}} />
-                <br/>
-                @error('surname') <span class="error">{{ $message }}</span> @enderror
+                <x-textfield :label="__('Surname')" type="text" id="surname" wire:model="surname" />
                 <br/>
 
-                <x-textfield :label="__('Email')" type="text" id="email" {{--wire:model="username"--}} />
-                <br/>
-                @error('email') <span class="error">{{ $message }}</span> @enderror
+                <x-textfield :label="__('Email')" type="text" id="email" wire:model="email" />
                 <br/>
 
-                <x-textfield :label="__('Date of birth')" type="date" id="dateofbirthsignup" {{--wire:model="username"--}} />
-                <br/>
-                @error('dateofbirth') <span class="error">{{ $message }}</span> @enderror
+                <x-textfield :label="__('Date of birth')" type="date" id="dateofbirthsignup" wire:model="dateOfBirth" />
                 <br/>
 
-
-                <x-textfield :label="__('Username')" type="text" id="username" {{--wire:model="username"--}} />
-                <br/>
-                @error('username') <span class="error">{{ $message }}</span> @enderror
+                <x-textfield :label="__('Username')" type="text" id="username" wire:model="username" />
                 <br/>
 
-                <x-textfield :label="__('Password')" type="password" required id="password"  />
-                <br/>
-                @error('password') <span class="error">{{ $message }}</span> @enderror
+                <x-textfield :label="__('Password')" type="password" required id="password" wire:model="password" />
                 <br/>
 
-                <x-textfield :label="__('Repeat password')" type="password" required id="repeat-password"  />
-                <br/>
-                @error('repeat-password') <span class="error">{{ $message }}</span> @enderror
+                <x-textfield :label="__('Repeat password')" type="password" required id="repeat-password"
+                                                  wire:model="repeatPassword" />
                 <br/>
 
                 <x-button :label="__('Signup')" id="signup-button" variant="raised"/>
             </form>
         </div>
     </div>
+    <x-button :label="__('Already have an account? Login')" wire:click="goToLogin"/>
+    <livewire:ui.language.select style="margin-top: 16px;"/>
 
     @if(session()->has('message'))
         <x-snackbar :message="session('message')" id="signupMessage" open/>
