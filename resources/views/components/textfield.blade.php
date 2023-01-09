a1f0c35f591056db0c1e563ca4d5320af1857039
Maicol Battistini <maicol.battistini@studio.unibo.it>
2023-01-09 19:33:40 +0100

    fix: Fix textfield error styles and character counter

--- resources/views/components/textfield.blade.php
+++ resources/views/components/textfield.blade.php
@@ -12,9 +12,16 @@
     'error' => ''
 ])
 
-<label wire:ignore.self class="mdc-text-field mdc-text-field--{{$outlined ? 'outlined' : 'filled'}}
+@php
+    $error = $errors->first($id) ?? $error;
+@endphp
+
+<label wire:ignore.self id="{{$id}}" class="mdc-text-field @if($error) mdc-text-field--invalid @endif mdc-text-field--{{$outlined ? 'outlined' : 'filled'}}
     @if($textarea) mdc-text-field--textarea mdc-text-field--with-internal-counter @endif
     @if($value) mdc-text-field--label-floating @endif">
+    @error($id)
+        <div class="mdc-text-field--invalid" aria-hidden="true"></div>
+    @enderror
     @if(!$outlined && !$textarea)
         <span class="mdc-floating-label @if($value) mdc-floating-label--float-above @endif" id="{{$id}}-label" wire:ignore.self>
             {{$label}}
@@ -40,11 +47,11 @@
                       value="{{$value}}"
                 {{$attributes}}></textarea>
             <span class="mdc-text-field-character-counter" wire:ignore>
-                0 / {{$maxlength}}
+                {{strlen($value)}} / {{$maxlength}}
             </span>
         </span>
     @else
-        <input class="mdc-text-field__input" type="{{$type}}" aria-labelledby="{{$id}}-label" value="{{$value}}" {{$attributes}} wire:ignore />
+        <input class="mdc-text-field__input" id="{{$id}}-input" type="{{$type}}" aria-labelledby="{{$id}}-label" @if($value) value="{{$value}}" @endif {{$attributes}} wire:ignore />
     @endif
     @empty($outlined)
         <div class="mdc-line-ripple" wire:ignore></div>
