68a8663dcd5bcaa504480961fc7a98682a22c8aa
Maicol Battistini <maicolbattistini@live.it>
2023-01-26 10:31:54 +0100

    New translations auth.php (Italian)

--- it/lang/it/auth.php.php
+++ it/lang/it/auth.php.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'These credentials do not match our records.',
-    'password' => 'The password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed'   => 'Credenziali non valide.',
+    'password' => 'Il campo :attribute non è corretto.',
+    'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
 ];
