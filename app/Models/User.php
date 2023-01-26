ba5ab70fdf67a1fc25faf08cb3fb86b54a57dd33
Maicol Battistini <maicol.battistini@studio.unibo.it>
2023-01-26 10:20:16 +0100

    chore: Removed Sanctum from user

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
