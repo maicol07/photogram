96bb67f7922c8b64bfbe8a98d7b267073c3e0ffd
Simone Redighieri <simone.redighieri@studio.unibo.it>
2023-01-26 10:07:04 +0100

    feat(components): Added top app bar component

---
+++ resources/views/components/top-app-bar.blade.php
@@ -0,0 +1,24 @@
+@props([
+    'id' => '',
+    'menuIcon' => '',
+    'title' => '',
+    'variants' => ''
+])
+
+<header class="mdc-top-app-bar app-bar @empty(!$variants) mdc-top-app-bar--{{$variants}} @endempty">
+    <div class="mdc-top-app-bar__row">
+        @empty(!$menuIcon)
+            <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                <x-button class="mdc-top-app-bar__navigation-icon" id="{{$id}}" outlined iconButton
+                              icon="{{$menuIcon}}"/>
+                <span class="mdc-top-app-bar__title">{{$title}}</span>
+            </section>
+        @endempty
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+            {{$buttons}}
+        </section>
+    </div>
+</header>
+<main class="mdc-top-app-bar--fixed-adjust">
+    {{$slot}}
+</main>
