66084c324802b4a0d90f9a4db902beac6582e62a
Elia Bellavista <elia.bellavista@studio.unibo.it>
2023-02-08 01:13:52 +0100

    fix(post): Fix wrong relationship type

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,15 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
-    public function user(): HasOne
+    public function user(): BelongsTo
     {
-        return $this->hasOne(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function comments(): HasMany
