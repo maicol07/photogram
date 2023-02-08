3088b130114395d7ed2af35f9813ed776c871c42
Maicol Battistini <maicol.battistini@studio.unibo.it>
2023-02-09 00:39:42 +0100

    fix: Image list not showing images

--- resources/views/components/image-list-item.blade.php
+++ resources/views/components/image-list-item.blade.php
@@ -1,10 +1,11 @@
 @props([
     'text' => '',
+    'src' => '',
     'alt' => ''
 ])
 
 <li {{$attributes->class('mdc-image-list__item')}}>
-    <img class="mdc-image-list__image" alt="{{$alt}}">
+    <img class="mdc-image-list__image" src="{{$src}}" alt="{{$alt}}">
     <div class="mdc-image-list__supporting">
         <span class="mdc-image-list__label">{{$text}}</span>
     </div>
