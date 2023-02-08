8f55faa9d85d1651b617b958771ec9394ede5dfc
Simone Redighieri <simone.redighieri@studio.unibo.it>
2023-02-08 15:47:16 +0100

    refactor(image-list): Improved image list attributes handling

--- resources/views/components/image-list-item.blade.php
+++ resources/views/components/image-list-item.blade.php
@@ -1,9 +1,10 @@
 @props([
-    'text' => ''
+    'text' => '',
+    'alt' => ''
 ])
 
-<li class="mdc-image-list__item">
-    <img class="mdc-image-list__image" {{$attributes}}>
+<li {{$attributes->class('mdc-image-list__item')}}>
+    <img class="mdc-image-list__image" alt="{{$alt}}">
     <div class="mdc-image-list__supporting">
         <span class="mdc-image-list__label">{{$text}}</span>
     </div>
--- resources/views/components/image-list.blade.php
+++ resources/views/components/image-list.blade.php
@@ -2,6 +2,6 @@
     'variant' => 'with-text-protection'
 ])
 
-<ul class="mdc-image-list mdc-image-list--{{$variant}} posts-list">
+<ul class="mdc-image-list mdc-image-list--{{$variant}} posts-list" {{$attributes}}>
     {{$slot}}
 </ul>
