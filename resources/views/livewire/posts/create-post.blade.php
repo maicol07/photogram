26ddef9dc427d2858ab8ea766a62b0850830f922
Elia Bellavista <elia.bellavista@studio.unibo.it>
2023-02-10 15:19:15 +0100

    style: Improved post style

--- resources/views/livewire/posts/create-post.blade.php
+++ resources/views/livewire/posts/create-post.blade.php
@@ -1,19 +1,18 @@
-<form wire:submit.prevent="upload" class="post-create-container">
-    <div class="mdc-card mdc-card--outlined post-create-card" id="post-create-card" >
+<form wire:submit.prevent="upload">
+    <x-card outlined class="container-card">
         <div>
             <span>
                     @lang('Add an image to your post'):
             </span>
             <div>
-                <x-button id="attachment-btn" outlined :label="__('Upload File')" icon="upload" type="button" />
+                <x-button id="upload-image-button" outlined :label="__('Upload File')" icon="upload" type="button" />
             </div>
             <input wire:model="image" id="add-image" name="image" type="file"
                    accept=".jpg,.jpeg,.png,.gif,.bmp,.svg,.webp" />
         </div>
-        @error('image')<span>{{ $message }}</span>@enderror
 
         @if ($image && !$errors->has('image'))
-            <div>
+            <div style="text-overflow: ellipsis; overflow: clip">
                 @lang('Uploaded file'): {{($image instanceof \Livewire\TemporaryUploadedFile)? $image->getClientOriginalName() : $image}}
             </div>
             <br/>
@@ -34,17 +33,29 @@
                          id="edit-bio" :label="__('Add a description')"/>
         </div>
         <div class="mdc-dialog__actions">
-            <x-button dialog-button :label="__('Delete')" variant="text" wire:click="delete" />
+            @if($this->post->exists)
+                <x-button dialog-button icon="delete" :label="__('Delete')" variant="text" wire:click="delete" />
+            @endif
             <x-button dialog-button :label="__('Cancel')" variant="text" wire:click="goToProfile" />
             <x-button dialog-button :label="__('OK')" variant="text" type="submit" />
         </div>
-    </div>
+    </x-card>
     <script wire:ignore>
         /** @type {HTMLButtonElement} */
-        const button = document.querySelector('button#attachment-btn');
+        const button = document.querySelector('button#upload-image-button');
         button.addEventListener('click', () => {
-            document.querySelector('input#add-image')
-                ?.click();
+          document.querySelector('input#add-image')
+            ?.click();
         });
     </script>
+
+    <x-snackbar id="imageError"></x-snackbar>
+    @error('image')
+        <script wire:key="{{now()}}">
+            /** @type{import('@material/snackbar').MDCSnackbar} */
+            const snackbar = window.mdc.snackbar.imageError;
+            snackbar.labelText = @js($message);
+            snackbar.open();
+        </script>
+    @enderror
 </form>
--- resources/views/livewire/posts/post-view.blade.php
+++ resources/views/livewire/posts/post-view.blade.php
@@ -33,16 +33,7 @@
                             <x-button id="edit-post-button" label="edit post" wire:click="editPost"
                                       variant="outlined" icon="pencil" />
                         @endif
-
-
-                        <span class="mdc-menu-surface--anchor">
-                            <x-button id="share-button" iconButton wire:click="share" icon="share-variant-outline"  />
-
-                            <x-menu-surface class='share-menu' id="share-menu-{{$this->post->id}}" >
-                                <span> share link:&nbsp </span> <a> {{route('inside.viewPost', $this->post->id)}}</a>
-                                <x-button id="copy-share-link-button" iconButton icon="content-copy" />
-                            </x-menu-surface>
-                        </span>
+                            <x-button id="share-button-{{$post->id}}" iconButton wire:click="share" icon="share-variant-outline"/>
                     </div>
                 </div>
 
@@ -56,7 +47,7 @@
 
         <form wire:submit.prevent="addComment"  id="add-comment-section" >
             <div id="comment-textfield-counter">
-                <x-textfield id="add-comment-content" :label="$this->editMode ? (__('Edit comment')) : (__('Add a comment'))"
+                <x-textfield class="add-comment-content" id="add-comment-content-{{$post->id}}" :label="$this->editMode ? (__('Edit comment')) : (__('Add a comment'))"
                              wire:model="commentContent" maxlength="255"/>
             </div>
             <x-button type="submit" id="add-comment-button" icon="send-outline" icon-button />
@@ -74,11 +65,16 @@
         <livewire:user.dialog-user-list :userList="$this->post->likes"/>
     </x-dialog>
 
+    <x-menu-surface class='share-menu' id="share-menu-{{$this->post->id}}" anchor-id="share-button-{{$post->id}}" fixed>
+        <span>@lang('Share link'):&nbsp</span> <a> {{route('inside.viewPost', $this->post->id)}}</a>
+        <x-button id="copy-share-link-button-{{$this->post->id}}" iconButton icon="content-copy" />
+    </x-menu-surface>
+
     <script wire:ignore>
-        window.addEventListener('edit-comment', () => {
-          document.querySelector('#add-comment-content').focus();
+        window.addEventListener('edit-comment-{{$this->post->id}}', () => {
+          document.querySelector('#add-comment-content-{{$this->post->id}}').focus();
         });
-        document.querySelector('#copy-share-link-button').addEventListener('click', () =>{
+        document.querySelector('#copy-share-link-button-{{$this->post->id}}').addEventListener('click', () =>{
             navigator.clipboard.writeText(@js(route('inside.viewPost', $this->post->id)));
         });
     </script>
