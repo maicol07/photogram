b2b9e3998b535beeeb492f69a4132961ae165344
Elia Bellavista <elia.bellavista@studio.unibo.it>
2023-02-11 19:26:15 +0100

    feat: ✨ Added missing comment component

---
+++ app/Http/Livewire/Posts/CommentComponent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Livewire\Posts;
+
+use App\Http\Livewire\Traits\MDCDialogFeatures;
+use App\Http\Livewire\Traits\MDCMenuFeatures;
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class CommentComponent extends Component
+{
+    use MDCDialogFeatures;
+    use MDCMenuFeatures;
+
+    public Comment $c;
+    public Post $post;
+
+    public function commentLikeToggle(int $commentId): void //TODO
+    {
+        $this->post->comments()->find($commentId)->likes()->toggle(Auth::user()->id);
+    }
+
+    public function goToProfile(): void
+    {
+        $this->redirectRoute('inside.profile', $this->c->user->username);
+    }
+
+    public function deleteComment(int $commentId): void
+    {
+        $this->c->delete();
+        $this->emit('refreshPostView');
+    }
+
+    public function editComment(): void
+    {
+        $this->emitUp('editComment', $this->c);
+        $this->dispatchBrowserEvent("edit-comment-{$this->post->id}");
+    }
+
+    public function render(): View
+    {
+        return view('livewire.posts.comment-component');
+    }
+}
