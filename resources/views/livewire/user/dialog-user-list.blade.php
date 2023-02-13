212d474d9d0d32522072ecf0b7eb2f68a79120c3
Maicol Battistini <maicol.battistini@studio.unibo.it>
2023-02-13 22:21:36 +0100

    fix: Fix profile username redirect with special characters

--- resources/views/livewire/user/dialog-user-list.blade.php
+++ resources/views/livewire/user/dialog-user-list.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-list>
         @foreach($userList as $user)
-            <x-list-item text="{{$user->username}}" secondary-text="{{$user->bio}}" href="/profile/{{$user->username}}">
+            <x-list-item :text="$user->username" :secondary-text="$user->bio" :href="route('inside.profile', $user->username)">
                 <x-slot:graphic>
                     @if(!$user->profileImage)
                         <span class="mdi mdi-account-circle mdc-button__icon dialog-icon" role="img" aria-label="{{__(":user's profile image", ['user' => $user->username])}}"></span>
