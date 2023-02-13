988edab98af77a46cc153e02e2885e3fde5dad45
Maicol Battistini <maicol.battistini@studio.unibo.it>
2023-02-13 10:26:50 +0100

    fix: 🐛 Fix verify email

--- app/Http/Livewire/Email/VerifyEmail.php
+++ app/Http/Livewire/Email/VerifyEmail.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Livewire\Email;
 
-use App\Http\Livewire\Page;
+use App\Http\Livewire\AuthPage;
 use Illuminate\Contracts\View\View;
 
-class VerifyEmail extends Page
+class VerifyEmail extends AuthPage
 {
     public function page(): View
     {
         return view('livewire.email.verify-email');
     }
 
+    public function getTitle(): string
+    {
+        return __('Verify Email');
+    }
+
     public function resendEmail(): void
     {
-        request()->user()->sendEmailVerificationNotification();
+        request()?->user()->sendEmailVerificationNotification();
         $this->openSnackbar('resendEmail', __('An email has been sent!'));
-        //TODO Throttle: https://github.com/danharrin/livewire-rate-limiting })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
     }
 }
--- resources/views/livewire/email/verify-email.blade.php
+++ resources/views/livewire/email/verify-email.blade.php
@@ -1,5 +1,5 @@
-<div>
-    <span> @lang('To continue please click the link that has been sent to your email.') </span>
+<div id="verify-email-container">
+    <p>@lang('To continue please click the link that has been sent to your email.')</p>
     <x-button :label="__('Did not receive an email? Send again')" wire:click="resendEmail"/>
     <x-snackbar id="resendEmail" />
 </div>
